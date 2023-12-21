b2bd8263b66688e03755af13819417b42232bf89
Urso-Suzi <pedroviniciussouza24@gmail.com>
2023-12-20 22:13:37 -0300

    trazendo imagens da API

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -2,10 +2,16 @@
 
 const PokeApi = {}
 
-PokeApi.getPokemons =  (offset = 10, limit = 10) => {
+PokeApi.getPokemonsDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
+PokeApi.getPokemons =  (offset = 1, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.error(error))
+    .then((pokemons) => pokemons.map(PokeApi.getPokemonsDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonDetails) => pokemonDetails)
 }
